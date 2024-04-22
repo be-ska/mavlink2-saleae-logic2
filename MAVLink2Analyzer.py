2c8f1faaedc0b51540cbbd03343836ca6a953d94
Luca Pescante <72148330+be-ska@users.noreply.github.com>
2024-04-23 00:30:29 +0200

    first commit

---
+++ MAVLink2Analyzer.py
@@ -0,0 +1,71 @@
+# MAVLink 2 High Level Analyzer
+
+from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame, StringSetting, NumberSetting, ChoicesSetting
+import generated_mavlink2_all as mavlink
+
+class MAVLink2(HighLevelAnalyzer):
+    # Global variables
+    mav = mavlink.MAVLink(None)
+    buff = bytearray()
+    packet_length = 0
+    parse_state = 0
+    message = None
+    error = None
+    start_frame = None
+    header = b'\xfd'
+
+    # An optional list of types this analyzer produces, providing a way to customize the way frames are displayed in Logic 2.
+    result_types = {
+        'mavlink': {
+            'format': 'sys ID: {{data.sys_id}}, comp ID: {{data.comp_id}}, msg ID: {{data.msg_id}}'
+        }
+    }
+
+    def __init__(self):
+        pass
+    
+    # parse a byte
+    def parse_mavlink(self, byte):
+        if self.parse_state == 0:
+            # wait header
+            if byte == self.header:
+                self.buff.clear()
+                self.buff += byte
+                self.parse_state = 1
+                return 1
+        elif self.parse_state == 1:
+            # check packet length
+            self.packet_length = int.from_bytes(byte, "big")
+            self.buff += byte
+            self.parse_state = 2
+        elif self.parse_state == 2:
+            self.buff += byte
+            if len(self.buff) > self.packet_length+11:
+                self.parse_state = 0
+                msg = None
+                try:
+                    msg = self.mav.decode(self.buff)
+                except mavlink.MAVError as e:
+                    print("MAVLink parsing error:", e)
+                    return 2
+                if msg is not None:
+                    self.message = msg
+                    return 0
+        return 3
+                
+    def decode(self, frame: AnalyzerFrame):
+        # receive incoming byte from Logic
+        res = self.parse_mavlink(frame.data['data'])
+        if res == 0:
+            # message parsed
+            return AnalyzerFrame('mavlink', self.start_frame, frame.end_time, {
+                'sys_id': self.message.get_srcSystem(),
+                'comp_id': self.message.get_srcComponent(),
+                'msg_id': self.message.get_msgId(),
+                'payload' : self.message.get_payload(),
+                'print' : str(self.message)
+            })
+        elif res == 1:
+            self.start_frame = frame.start_time
+
+        
---
+++ README.md
@@ -0,0 +1,23 @@
+
+# MAVLink 2
+  
+## Getting started
+
+pymavlink generates the Python 3 mavlink binding from the mavlink submodule. To generate it first install pymavlink
+```
+$ python3 -m pip install pymavlink
+```
+and then generate the python MAVLink 2 dependencies
+```
+$ mavgen.py --wire-protocol 2.0 --lang Python3 modules/mavlink/message_definitions/v1.0/all.xml -o generated_mavlink2_all.py
+``` 
+
+## Usage
+1. Inspect the serial data using `Async Serial` analyzer. Be sure to set the right baud rate and channel:
+1. Add MAVLink2 analyzer, specifying the Input Analyzer;
+1. both on Data Table and plot view you can see the decoding data;
+1. if there are some parsing errors, they are printed on Analyzer terminal;
+1. for an easier view set the datatype as Decimal.
+
+
+![set decimal datatype](set_decimal.png)
