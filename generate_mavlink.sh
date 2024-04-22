fe00cfcddd862cf3ffe617b3972ca6176938786c
Luca Pescante <72148330+be-ska@users.noreply.github.com>
2024-04-23 00:36:19 +0200

    remove shell script

--- generate_mavlink.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# re-generate mavlink headers, assumes pymavlink is installed
-
-echo "Generating mavlink2 headers"
-rm -f generated_mavlink2_all.py
-mavgen.py --wire-protocol 2.0 --lang Python3 modules/mavlink/message_definitions/v1.0/all.xml -o generated_mavlink2_all.py
